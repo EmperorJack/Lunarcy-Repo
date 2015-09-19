fc2a00d162b43ef8a4e6a0d2331c0b5fa3f4a8fb
Ben Evans <ben.evans_9@hotmail.com>
2015-09-19 15:40:51 +1200

    Added confirmation before exit, and center aligning window in frame

--- Lunarcy/src/user_interface/Frame.java
+++ Lunarcy/src/user_interface/Frame.java
@@ -1,10 +1,16 @@
 package user_interface;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.*;
+
+
 
 @SuppressWarnings("serial")
 public class Frame extends JFrame {
@@ -15,8 +21,20 @@ public class Frame extends JFrame {
 	public Frame() {
 		setTitle("Lunarcy");
 		setSize(INIT_WIDTH, INIT_HEIGHT);
-		// TODO catch exit with confirmation dialog
-		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+		
+		//Catch exit with confirmation dialog
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e){
+				int option = JOptionPane.showConfirmDialog(null, new JLabel("Are you sure you want to exit Lunarcy?"),
+						"Exit Game", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+				//Only close if they confirm they want to close
+				if (option == JOptionPane.YES_OPTION) {
+					System.exit(0);
+				}
+			}
+		});
+		
 
 		// setup processing canvas panel
 		JPanel panel = new JPanel(new BorderLayout());
@@ -35,6 +53,11 @@ public class Frame extends JFrame {
 						.getComponent().getHeight());
 			}
 		});
+		
+		//Center align the frame on screen
+		Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+		setBounds((size.width - getWidth()) / 2, (size.height - getHeight()) / 2, getWidth(), getHeight());
+
 
 		setVisible(true);
 	}
