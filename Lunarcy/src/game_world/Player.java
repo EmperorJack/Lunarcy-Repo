004f7b9ea9e28158a52dbaf472a8f83013369dc9
RobertDIversen <robertdiversen@gmail.com>
2015-09-21 18:59:08 +1200

    added more fields to Player

--- Lunarcy/src/game_world/Player.java
+++ Lunarcy/src/game_world/Player.java
@@ -1,6 +1,22 @@
 package game_world;
 
+import java.util.Set;
+
 public class Player {
-	Location location;
-	Direction facing;
+	private final int id;
+	private final String name;
+	// Unsure about type
+	//private Color color;
+	
+	private Location location;
+	private Direction facing;
+	private int oxygen;
+	// Unsure about type
+	private Set<Item> inventory;
+	
+	public Player(int uniqueID, String name){
+		this.id = uniqueID;
+		this.name = name;
+		this.oxygen = 200;
+	}
 }
