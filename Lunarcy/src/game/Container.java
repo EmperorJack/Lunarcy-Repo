20a9e56ba273ca1e757c1df47d27d815833208aa
RobertDIversen <robertdiversen@gmail.com>
2015-09-29 12:30:07 +1300

    Last commit didn't work, fixed now

---
+++ Lunarcy/src/game/Container.java
@@ -0,0 +1,13 @@
+package game;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public abstract class Container extends Entity {
+	Set<Item> items;
+	public Container(int itemID) {
+		super(itemID);
+		items = new HashSet<Item>();
+	}
+
+}
