9441288ec5a9061d204e34ecfbd161209d4d55c9
Jack Purvis <purvisjack@la-normandie.ecs.vuw.ac.nz>
2015-09-30 15:58:44 +1300

    Reverted constructor change in frame / canvas as unrequired. Disabled party lights for integration

--- Lunarcy/src/ui/Perspective3D.java
+++ Lunarcy/src/ui/Perspective3D.java
@@ -26,7 +26,6 @@ public class Perspective3D extends DrawingComponent {
 	private final int SQUARE_SIZE = 500;
 	private final float MODEL_SCALE = SQUARE_SIZE / 2.5f;
 	private final int vel = SQUARE_SIZE / 50;
-	private int r, g, b;
 
 	// player fields
 	private int playerID;
@@ -92,12 +91,7 @@ public class Perspective3D extends DrawingComponent {
 		// test light source and sphere
 		p.pushMatrix();
 		p.translate(500, 500, SQUARE_SIZE / 2);
-		if (p.frameCount % 60 == 0) {
-			r = (int) p.random(200) + 55;
-			g = (int) p.random(200) + 55;
-			b = (int) p.random(200) + 55;
-		}
-		p.pointLight(r, g, b, 0, 0, 0);
+		p.pointLight(255, 255, 255, 0, 0, 0);
 		p.fill(0, 0, 255);
 		p.sphere(10);
 		p.popMatrix();
