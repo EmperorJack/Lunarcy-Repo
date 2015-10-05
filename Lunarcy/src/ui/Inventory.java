f07e02e5b5bf28ccb5047f7cb2a42150c0d8df7c
Ben Evans <ben.evans_9@hotmail.com>
2015-10-05 22:15:44 +1300

    Added darkness when oxygen is low, and skeleton code for clicking on items added

--- Lunarcy/src/ui/Inventory.java
+++ Lunarcy/src/ui/Inventory.java
@@ -1,5 +1,7 @@
 package ui;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.List;
 
 import game.GameState;
@@ -13,39 +15,48 @@ import processing.core.PApplet;
  * @author b
  *
  */
-public class Inventory extends DrawingComponent {
+public class Inventory extends DrawingComponent implements MouseListener {
 
 	// Where the Inventory bar starts (x)
 	private final int LEFT_PADDING = 25;
 	// Where the Inventory bar starts (y)
 	private final int TOP_PADDING = (int) (p.height * 0.8);
+	private final int INVENTORY_WIDTH = (int) (p.width * 0.3);
 
 	// Size of each item
 	private final int SIZE = 35;
 	// Space between each item
 	private final int SPACING = 20;
 
+	private final int MENU_SIZE = 300;
+	private boolean drawMenu;
+
+	private List<Item> inventory;
+
 	public Inventory(PApplet p, GameState gameState, int playerID) {
 		super(p, gameState, playerID);
+		inventory = gameState.getPlayer(playerID).getInventory();
+
+		p.addMouseListener(this);
 	}
 
 	@Override
 	public void draw(GameState gameState, float delta) {
-		// get the player from the current game state
-		Player player = gameState.getPlayer(playerID);
+		// Get all the players items
+		inventory = gameState.getPlayer(playerID).getInventory();
 
 		p.pushMatrix();
 		p.pushStyle();
 
-		p.stroke(1);
+		p.noStroke();
 
 		// Translate drawing to match location
 		p.translate(LEFT_PADDING, TOP_PADDING);
 
-		// Draw all the players items
-		List<Item> inventory = player.getInventory();
+		// Draw the background
+		p.fill(0, 0, 0, 100);
+		p.rect(0, 0, INVENTORY_WIDTH, SIZE);
 
-		p.textSize(SIZE);
 		p.noFill();
 
 		if (inventory != null) {
@@ -56,9 +67,94 @@ public class Inventory extends DrawingComponent {
 			}
 		}
 
+		if (drawMenu) {
+			drawMenu();
+		}
+
 		// pop matrix and style information from the stack
 		p.popStyle();
 		p.popMatrix();
+	}
+
+	/**
+	 * Returns the item at the clicked position, or null if the click was not on
+	 * a valid item.
+	 * 
+	 * @param x
+	 *            : The clicked x location
+	 * @param y
+	 *            : The clicked y location
+	 * @return
+	 */
+	private Item getItemAt(int x, int y) {
+		// Width of each item
+		int width = SIZE + SPACING;
+		// Where the player clicked
+		int clickedX = x - LEFT_PADDING;
+		// Which item this corresponds to in our Set
+		int index = clickedX / width;
+
+		// Make sure our index is within bounds
+		if (index < 0 || index > inventory.size()) {
+			return null;
+		}
+
+		return inventory.get(index);
+	}
+
+	private void drawMenu() {
+		p.pushMatrix();
+
+		p.translate(-LEFT_PADDING, -TOP_PADDING); // Back to 0,0
+		p.fill(0, 0, 0, 100);
+		p.rect(p.width / 2 - MENU_SIZE / 2, p.height / 2 - MENU_SIZE / 2, MENU_SIZE, MENU_SIZE);
+
+		p.popMatrix();
+	}
+
+	/**
+	 * Returns true if the coordinates entered are inside the inventory.
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	private boolean withinBounds(int x, int y) {
+		return x > LEFT_PADDING && x < LEFT_PADDING + INVENTORY_WIDTH && y > TOP_PADDING && y < TOP_PADDING + SIZE;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// Get the mouses coordinates
+		int x = e.getX();
+		int y = e.getY();
+
+		// Only find the item if the click was on the inventory
+		if (withinBounds(x, y)) {
+			Item item = getItemAt(x, y);
+
+			if (item != null) {
+				// Show display
+				drawMenu = true;
+			}
+		}
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
 	}
 
 }
--- Lunarcy/src/ui/Oxygen.java
+++ Lunarcy/src/ui/Oxygen.java
@@ -12,7 +12,6 @@ import processing.core.PApplet;
  */
 public class Oxygen extends DrawingComponent {
 
-
 	// Where the oxygen bar starts (x)
 	private final int LEFT_PADDING = (int) (p.width * 0.8);
 	// Where the oxygen bar starts (y)
@@ -21,11 +20,14 @@ public class Oxygen extends DrawingComponent {
 	// Sizing of the oxygen tank
 	private final int CYLINDERWIDTH = 200;
 	private final int CYLINDERHEIGHT = 35;
-
-	private final int LOW_OXYGEN = 50;
-
 	// Size of the dial on the oxygen tank
 	private final int DIALSIZE = 10;
+	
+	private final int LOW_OXYGEN = 50;
+	
+	private int dimAmount = 0;
+	
+	
 
 	public Oxygen(PApplet p, GameState gameState, int playerID) {
 		super(p, gameState, playerID);
@@ -54,18 +56,19 @@ public class Oxygen extends DrawingComponent {
 		p.rectMode(p.CORNERS);
 		p.noStroke();
 
-		//Set the colour to red if there is little oxygen left
+		// Set the colour to red if there is little oxygen left
+		
 
-		if(oxygen < LOW_OXYGEN){
+		if (oxygen < LOW_OXYGEN) {
+			dimScreen();
 			p.fill(255, 0, 0, 100);
-		}
-		else{
+		} else {
 			p.fill(139, 158, 162);
 		}
 
-		p.rect(CYLINDERWIDTH-oxygen, 0, CYLINDERWIDTH, CYLINDERHEIGHT);
+		p.rect(CYLINDERWIDTH - oxygen, 0, CYLINDERWIDTH, CYLINDERHEIGHT);
 
-		//Set our drawing values back
+		// Set our drawing values back
 		p.rectMode(p.CORNER);
 		p.fill(139, 158, 162);
 
@@ -85,6 +88,16 @@ public class Oxygen extends DrawingComponent {
 		// pop matrix and style information from the stack
 		p.popStyle();
 		p.popMatrix();
+	}
+
+	/**
+	 * Darkens the screen to show the player they are low on oxygen
+	 */
+	private void dimScreen() {
+		p.fill(0, 0, 0, dimAmount);
+		p.rect(-LEFT_PADDING, -TOP_PADDING, p.width, p.height);
+		dimAmount += 2;
+		dimAmount = Math.max(0, dimAmount);
 	}
 
 }
