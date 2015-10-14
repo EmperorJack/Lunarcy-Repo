745bf5b943bc62fb03487237c5c0642d35b9828b
EmperorJack <emperorpurvis@gmail.com>
2015-10-14 19:44:40 +1300

    Added monitor furniture image

--- Lunarcy/src/game/Monitor.java
+++ Lunarcy/src/game/Monitor.java
@@ -2,11 +2,11 @@ package game;
 
 public class Monitor implements Furniture {
 	private static final long serialVersionUID = -3134459471570672979L;
-	
+
 	public Monitor(){
 	}
 	public String getImageName() {
-		return "lockedChest_open";
+		return "monitor";
 	}
 
 }
